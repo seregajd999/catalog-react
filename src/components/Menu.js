2245a2d7b66feb4cf722b108f3d53c74f3d17c9a
alexbaumgertner <alex.baumgertner@gmail.com>
2016-09-25 17:03:00 +0200

    Move menu data to State

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
+import React, {
+  Component,
+  PropTypes
+} from 'react';
+
 import { Link }  from 'react-router';
 
 import './Menu.css';
 
+
 class Menu extends Component {
+
   render() {
+    let items = this.props.items.map((item) => {
+      return (
+        <li key={item.url}>
+          <Link to={item.url}>{item.name}</Link>
+        </li>
+        );
+    });
+
     return (
       <nav className="Menu">
-        <ul>
-          <li>
-            <Link to="/">Главная</Link>
-          </li>
-          <li>
-            <Link to="/catalog">Каталог</Link>
-          </li>
-          <li>
-            <Link to="/cart">Корзина</Link>
-          </li>
-        </ul>
+        <ul>{items}</ul>
       </nav>
     );
   }
 }
+
+Menu.propTypes = {
+  items: PropTypes.array
+};
 
 export default Menu;
