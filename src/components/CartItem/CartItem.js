efa8102b93e0e828e3f34c97d309f287371636ce
alexbaumgertner <alex.baumgertner@gmail.com>
2016-09-27 16:49:22 +0200

    Add increase/decrease Cart controls

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -10,6 +10,42 @@ import './CartItem.css';
 
 
 class CartItem extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quantity: this.props.item.quantity
+    };
+
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+  }
+
+  increase() {
+    let item = this.props.item;
+
+    item.quantity = item.quantity + 1;
+
+    this.setState({
+      quantity: item.quantity
+    });
+  }
+
+  decrease() {
+    let item = this.props.item;
+
+     console.log(item.quantity);
+
+     if (item.quantity >= 1) {
+       item.quantity = item.quantity - 1;
+     }
+
+    this.setState({
+      quantity: item.quantity
+    });
+  }
+
   render() {
     let item = this.props.item;
 
@@ -17,8 +53,14 @@ class CartItem extends Component {
       <tr className="CartItem">
         <td className="name"><Link to={'/catalog/' + item._id}>{item.name}</Link></td>
         <td className="price">{item.price}</td>
-        <td className="quantity">{item.quantity}</td>
-        <td className="price-total">{parseInt(item.price, 10) * item.quantity}</td>
+        <td className="quantity">
+          <div>{this.state.quantity}</div>
+          <div className="controls">
+            <button className="increase" onClick={this.increase}>+</button>
+            <button className="decrease" onClick={this.decrease}>-</button>
+          </div>
+        </td>
+        <td className="price-total">{parseInt(item.price, 10) * this.state.quantity}</td>
       </tr>
     );
   }
