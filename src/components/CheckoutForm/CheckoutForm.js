09ab3b5ffc6fe3091de49b93668c8ee26ca24302
alexbaumgertner <alex.baumgertner@gmail.com>
2016-09-30 00:33:52 +0200

    Add checkout form handlers stub

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -10,35 +10,83 @@ class CheckoutForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      client: {}
-    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAddressChange = this.handleAddressChange.bind(this);
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.handleInfoChange = this.handleInfoChange.bind(this);
+
+    this.state = {};
   }
 
   componentDidMount() {
-    this.setState({
-      client: this.props.client
-    });
+    this.setState(Object.assign({}, this.props.client));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("e: ", e);
+  }
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
+
+    console.log("this.state : ", this.state );
+  }
+
+  handleAddressChange(e) {
+    this.setState({ address: e.target.value });
+  }
+
+  handlePhoneChange(e) {
+    this.setState({ phone: e.target.value });
+  }
+
+  handleInfoChange(e) {
+    this.setState({ info: e.target.value });
   }
 
   render() {
-    let client = this.state.client;
 
     return (
-      <form className="CheckoutForm">
+      <form
+        onSubmit={this.handleSubmit}
+        className="CheckoutForm">
 
         <ul>
-          <li><input type="text" name="name" placeholder="Ваше имя" value={client.value} /></li>
-          <li><input type="text" name="address" placeholder="Ваш адрес" value={client.address} /></li>
-          <li><input type="text" name="phone" placeholder="Ваш телефон" value={client.phone}/></li>
+          <li><input
+            type="text"
+            onChange={this.handleNameChange}
+            name="name"
+            placeholder="Ваше имя"
+            value={this.state.name}/>
+          </li>
+
+          <li><input
+            type="text"
+            onChange={this.handleAddressChange}
+            name="address"
+            placeholder="Ваш адрес"
+            value={this.state.address}/>
+          </li>
+
+          <li><input
+            type="text"
+            onChange={this.handlePhoneChange}
+            name="phone"
+            placeholder="Ваш телефон"
+            value={this.state.phone}/></li>
+
           <li>
             <textarea
               name="info"
+              onChange={this.handleInfoChange}
               placeholder="Дополнительная информация: удобнее время доставки и тд."
-              value={client.info}
-            />
+              value={this.state.info}/>
           </li>
         </ul>
+
+        <button type="submit">Подтвердить</button>
 
       </form>
     );
